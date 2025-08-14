21dbc30bab27cb74a81c9f15b9364d78fd0d9ad8
Towa <towa.q@algaecal.com>
2025-08-14 16:19:58 -0700

    generated unique id for rich text toolbar editors

--- src/plugins/list-item/components/Toolbar.jsx
+++ src/plugins/list-item/components/Toolbar.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
+import React from "react";
 
-const Toolbar = ({ insertHighlight, insertColor }) => {
-    return (
-        <div id="toolbar" style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <div>
-                    <button className="ql-bold" />
-                    <button className="ql-italic" />
-                    <button className="ql-underline" />
-                    <button className="ql-strike" />
-                    <button className='ql-script' value="sub" />
-                    <button className='ql-script' value="super" />
-                    <button className="ql-link" />
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                    <button style={{ width: 'fit-content' }} className='ql-highlight' onClick={() => insertHighlight()}> <strong> Highlight </strong> </button>
-                    <button style={{ width: 'fit-content' }} className='ql-color' onClick={() => insertColor('red')}> <strong> Color </strong> </button>
-                </div>
-                <div>
-                    <button className="ql-list" value="ordered"></button>
-                    <button className="ql-list" value="bullet"></button>
-                    <button className="ql-indent" value="-1"></button>
-                    <button className="ql-indent" value="+1"></button>
-                </div>
-            </div>
+const Toolbar = ({ id, insertHighlight, insertColor }) => {
+  return (
+    <div
+      id={id}
+      style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div>
+          <button className="ql-bold" />
+          <button className="ql-italic" />
+          <button className="ql-underline" />
+          <button className="ql-strike" />
+          <button className="ql-script" value="sub" />
+          <button className="ql-script" value="super" />
+          <button className="ql-link" />
         </div>
-    )
-}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <button
+            style={{ width: "fit-content" }}
+            className="ql-highlight"
+            onClick={() => insertHighlight()}
+          >
+            {" "}
+            <strong> Highlight </strong>{" "}
+          </button>
+          <button
+            style={{ width: "fit-content" }}
+            className="ql-color"
+            onClick={() => insertColor("red")}
+          >
+            {" "}
+            <strong> Color </strong>{" "}
+          </button>
+        </div>
+        <div>
+          <button className="ql-list" value="ordered"></button>
+          <button className="ql-list" value="bullet"></button>
+          <button className="ql-indent" value="-1"></button>
+          <button className="ql-indent" value="+1"></button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Toolbar
+export default Toolbar;
--- src/plugins/list-item/index.jsx
+++ src/plugins/list-item/index.jsx
@@ -1,40 +1,41 @@
 // contents of plugin.jsx
 /** @jsx jsx */
-import { jsx } from '@emotion/core'; // Important! This line is necessary for the plugin to work with Builder.io 
-import { Builder } from '@builder.io/react';
-import ReactQuill, { Quill } from 'react-quill';
-import { useRef, useCallback } from 'react';
-import Toolbar from './components/Toolbar';
-import Link from '../../blots/Link';
-import Highlight from '../../blots/Highlight';
-import Color from '../../blots/Color';
+import { jsx } from "@emotion/core"; // Important! This line is necessary for the plugin to work with Builder.io
+import { Builder } from "@builder.io/react";
+import ReactQuill, { Quill } from "react-quill";
+import { useRef, useCallback, useState } from "react";
+import Toolbar from "./components/Toolbar";
+import Link from "../../blots/Link";
+import Highlight from "../../blots/Highlight";
+import Color from "../../blots/Color";
 
-const modules = {
-  toolbar: {
-    container: "#toolbar",
-  }
-};
+Quill.register({ "formats/mark": Highlight });
+Quill.register({ "formats/color": Color });
+Quill.register({ "formats/link": Link });
 
 const formats = [
-  'bold',
-  'italic',
-  'underline',
-  'strike',
-  'link',
-  'script',
-  'color',
-  'highlight',
-  'list',
-  'bullet',
-  'indent',
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "link",
+  "script",
+  "color",
+  "highlight",
+  "list",
+  "bullet",
+  "indent",
 ];
 
-Quill.register({ 'formats/mark': Highlight })
-Quill.register({ 'formats/color': Color });
-Quill.register({ 'formats/link': Link });
-
 const RichTextEditor = (props) => {
+  const [toolbarId] = useState(
+    () => `toolbar-${Math.random().toString(36).slice(2, 9)}`
+  );
   const editorRef = useRef(null);
+
+  const modules = {
+    toolbar: { container: `#${toolbarId}` },
+  };
 
   const insertHighlight = useCallback(() => {
     if (!editorRef.current) return;
@@ -42,36 +43,49 @@ const RichTextEditor = (props) => {
     const range = editor.getSelection();
     if (range) {
       const { highlight } = editor.getFormat(range);
-      editor.formatText(range.index, range.length, 'highlight', highlight);
+      editor.formatText(range.index, range.length, "highlight", highlight);
     }
   }, [editorRef]);
 
-  const insertColor = useCallback((color) => {
-    if (!editorRef.current) return;
-    const editor = editorRef.current.getEditor();
-    const range = editor.getSelection();
-    if (range) {
-      const { color: currentColor = 'red' } = editor.getFormat(range);
-      editor.formatText(range.index, range.length, 'color', currentColor ? false : color);
-    }
-  }, [editorRef]);
+  const insertColor = useCallback(
+    (color) => {
+      if (!editorRef.current) return;
+      const editor = editorRef.current.getEditor();
+      const range = editor.getSelection();
+      if (range) {
+        const { color: currentColor = "red" } = editor.getFormat(range);
+        editor.formatText(
+          range.index,
+          range.length,
+          "color",
+          currentColor ? false : color
+        );
+      }
+    },
+    [editorRef]
+  );
 
-
-  return <div className="text-editor">
-    <Toolbar insertHighlight={insertHighlight} insertColor={insertColor} />
-    <ReactQuill
-      ref={editorRef}
-      onChange={props.onChange}
-      value={props.value}
-      modules={modules}
-      formats={formats}
-      theme={"snow"}
-    />
-  </div>
-}
+  return (
+    <div className="text-editor">
+      <Toolbar
+        id={toolbarId}
+        insertHighlight={insertHighlight}
+        insertColor={insertColor}
+      />
+      <ReactQuill
+        ref={editorRef}
+        onChange={props.onChange}
+        value={props.value}
+        modules={modules}
+        formats={formats}
+        theme={"snow"}
+      />
+    </div>
+  );
+};
 
 Builder.registerEditor({
-  name: 'ListRichText',
+  name: "ListRichText",
   component: RichTextEditor,
 });
 
